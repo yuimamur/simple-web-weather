c8bad88ae71b57f5d8a8c388f9d63aa751cad26a
yuimamur <59761194+yuimamur@users.noreply.github.com>
2025-06-10 22:49:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -70,39 +70,3 @@ function getWeather() {
 
 
 
-
-//
-//document.getElementById('coordinates-form').addEventListener('submit', function (e) {
-//    e.preventDefault();
-//    const latInput = document.getElementById('lat-input');
-//    const lonInput = document.getElementById('lon-input');
-//    const lat = parseFloat(latInput.value);
-//    const lon = parseFloat(lonInput.value);
-//
-//    const MY_WEATHER_APIKEY = "e95aa28a8261b5084561dab6946047fc"; // ここにAPIキーを設定
-//
-//    const req = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${MY_WEATHER_APIKEY}`;
-//
-//    fetch(req)
-//        .then((response) => response.json())
-//        .then((data) => {
-//            console.log('API Response:', data);
-//            const weatherDescription = data.weather[0].description;
-//            const temperature = (data.main.temp - 273.15).toFixed(2); // ケルビンを摂氏に変換
-//            const humidity = data.main.humidity;
-//            const country = data.name;
-//            const weatherIcon = data.weather[0].icon; // アイコンコードを取得
-//
-//            // 受け取った値に応じて要素を更新
-//            document.getElementById('weather-description').textContent = `Weather: ${weatherDescription}`;
-//            document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
-//            document.getElementById('humidity').textContent = `Humidity: ${humidity}%`;
-//            document.getElementById('country').textContent = `Country: ${country}`;
-//            document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${weatherIcon}.png`;
-//
-//           // setWeatherIcon(humidity);
-//        })
-//        .catch((error) => {
-//            console.error(error);
-//        });
-//});
